e8e0ef4add30f5ecb9548f5a8f08a82bfa0c0795
AdamMambetov <adammambetov.342@gmail.com>
2025-10-16 00:36:56 +0300

    join random

--- src/api.gd
+++ src/api.gd
@@ -2,7 +2,11 @@ extends Node
 
 
 signal sign_result(result: bool)
+signal join_result(result: bool)
 
+
+var socket: WebSocketPeer
+var join_url: String
 
 @export var _request_path: NodePath
 @onready var request_node: HTTPRequest = get_node(_request_path)
@@ -11,7 +15,25 @@ const BASE_URL = "http://10.144.97.136:8000/"
 const header = {
 	sign_in = ["Content-Type: multipart/form-data; boundary=\"boundary\""],
 	sign_up = ["Content-Type: application/json"],
+	queue = ["Content-Type: application/json", "Authorization: Bearer %s"],
 }
+
+func _process(_delta: float) -> void:
+	if !is_instance_valid(socket):
+		return
+	socket.poll()
+	var state = socket.get_ready_state()
+	match state:
+		WebSocketPeer.STATE_OPEN:
+			print("STATE_OPEN")
+		WebSocketPeer.STATE_CLOSING:
+			print("STATE_CLOSING")
+		WebSocketPeer.STATE_CLOSED:
+			print("STATE_CLOSED")
+			var code = socket.get_close_code()
+			var reason = socket.get_close_reason()
+			socket.free()
+			socket = null
 
 # login
 func sign_in(username: String, password: String) -> void:
@@ -26,6 +48,15 @@ func sign_in(username: String, password: String) -> void:
 		HTTPClient.METHOD_POST,
 		body,
 	)
+	var res = await request_node.request_completed
+	if res[0] == HTTPRequest.RESULT_SUCCESS and res[1] == 200:
+		var body_json = JSON.parse_string(res[3].get_string_from_utf8())
+		Global.access = body_json.access
+		sign_result.emit(true)
+		print("request success! :)")
+	else:
+		sign_result.emit(false)
+		printerr("request error: ", res)
 
 # register
 func sign_up(username: String, email: String, password: String) -> void:
@@ -40,6 +71,35 @@ func sign_up(username: String, email: String, password: String) -> void:
 		HTTPClient.METHOD_POST,
 		data,
 	)
+	
+	var res = await request_node.request_completed
+	if res[0] == HTTPRequest.RESULT_SUCCESS and res[1] == 200:
+		emit_signal("sign_result", true)
+		print("request success! :)")
+	else:
+		emit_signal("sign_result", false)
+		printerr("request error: ", res)
+
+func join() -> void:
+	var headers = header.queue.duplicate(true)
+	headers[-1] %= Global.access
+	request_node.request(
+		BASE_URL + "queue/join/",
+		headers,
+		HTTPClient.METHOD_POST,
+	)
+	
+	var res = await request_node.request_completed
+	if res[0] == HTTPRequest.RESULT_SUCCESS and res[1] == 200:
+		join_result.emit(true)
+		print("request success! :)")
+		var body_json = JSON.parse_string(res[3].get_string_from_utf8())
+		var room_id = body_json.waiting_room_id
+		join_url = BASE_URL + room_id
+		_connect()
+	else:
+		join_result.emit(false)
+		printerr("request error: ", res)
 
 
 func _add_field(body: PackedByteArray, key: String, value: String) -> void:
@@ -49,14 +109,12 @@ func _add_field(body: PackedByteArray, key: String, value: String) -> void:
 func _end_body(body: PackedByteArray):
 	body.append_array("\r\n--boundary--\r\n".to_utf8_buffer())
 
-
-func _on_http_request_request_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
-	if result == HTTPRequest.RESULT_SUCCESS and response_code == 200:
-		var res = JSON.parse_string(body.get_string_from_utf8())
-		if res.has("access"):
-			Global.access = res.access
-		emit_signal("sign_result", true)
-		print("request success! :)")
+func _connect() -> void:
+	socket = WebSocketPeer.new()
+	var err = socket.connect_to_url(join_url)
+	if err != OK:
+		printerr("connect error: ", err)
+		await get_tree().create_timer(5).timeout
+		_connect()
 	else:
-		emit_signal("sign_result", false)
-		printerr("request error: ", result, " --- ", response_code)
+		printerr("connect...")
--- src/main_menu.gd
+++ src/main_menu.gd
@@ -5,7 +5,7 @@ var email: String
 var password: String
 
 func _on_start_btn_pressed() -> void:
-	pass # Replace with function body.
+	$GameMenuPopup.show()
 
 func _on_settings_btn_pressed() -> void:
 	pass # Replace with function body.
@@ -21,14 +21,12 @@ func _on_go_sign_up_btn_pressed() -> void:
 	$AccountsPopup/Background/SignUp.visible = true
 	username = $AccountsPopup/Background/SignUp/username_le.text
 	password = $AccountsPopup/Background/SignUp/password_le.text
-	print(username)
 
 func _on_go_sign_in_btn_pressed() -> void:
 	$AccountsPopup/Background/SignIn.visible = true
 	$AccountsPopup/Background/SignUp.visible = false
 	username = $AccountsPopup/Background/SignIn/username_le.text
 	password = $AccountsPopup/Background/SignIn/password_le.text
-	print(username)
 
 func _on_sign_up_btn_pressed() -> void:
 	$Api.sign_up(username, email, password)
@@ -53,3 +51,9 @@ func _on_email_le_text_changed(new_text: String) -> void:
 
 func _on_username_le_text_changed(new_text: String) -> void:
 	username = new_text
+
+func _on_play_random_btn_pressed() -> void:
+	$Api.join()
+
+func _on_play_friend_btn_pressed() -> void:
+	pass # Replace with function body.
