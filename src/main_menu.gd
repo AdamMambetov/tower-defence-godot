29f991277f1e137aa6afce6a1edee831ef5f543b
AdamMambetov <adammambetov.342@gmail.com>
2025-10-15 22:36:59 +0300

    update line edits in account menu

--- src/main_menu.gd
+++ src/main_menu.gd
@@ -1,5 +1,8 @@
 extends Control
 
+var username: String
+var email: String
+var password: String
 
 func _on_start_btn_pressed() -> void:
 	pass # Replace with function body.
@@ -16,26 +19,37 @@ func _on_exit_btn_pressed() -> void:
 func _on_go_sign_up_btn_pressed() -> void:
 	$AccountsPopup/Background/SignIn.visible = false
 	$AccountsPopup/Background/SignUp.visible = true
+	username = $AccountsPopup/Background/SignUp/username_le.text
+	password = $AccountsPopup/Background/SignUp/password_le.text
+	print(username)
 
 func _on_go_sign_in_btn_pressed() -> void:
 	$AccountsPopup/Background/SignIn.visible = true
 	$AccountsPopup/Background/SignUp.visible = false
+	username = $AccountsPopup/Background/SignIn/username_le.text
+	password = $AccountsPopup/Background/SignIn/password_le.text
+	print(username)
 
 func _on_sign_up_btn_pressed() -> void:
-	$Api.sign_up(
-		$AccountsPopup/Background/SignUp/username_te.text,
-		$AccountsPopup/Background/SignUp/email_te.text,
-		$AccountsPopup/Background/SignUp/password_te.text,
-	)
+	$Api.sign_up(username, email, password)
 	var res = await $Api.sign_result
 	if res:
 		$AccountsPopup.hide()
 
 func _on_sign_in_btn_pressed() -> void:
 	$Api.sign_in(
-		$AccountsPopup/Background/SignIn/username_te.text,
-		$AccountsPopup/Background/SignIn/password_te.text,
+		username,
+		password,
 	)
 	var res = await $Api.sign_result
 	if res:
 		$AccountsPopup.hide()
+
+func _on_password_le_text_changed(new_text: String) -> void:
+	password = new_text
+
+func _on_email_le_text_changed(new_text: String) -> void:
+	email = new_text
+
+func _on_username_le_text_changed(new_text: String) -> void:
+	username = new_text
