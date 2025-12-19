9a043b58f2f45054d4d6ec6f5a58ece4a56c6201
AdamMambetov <adammambetov.342@gmail.com>
2025-12-19 21:18:29 +0300

    fix tower

--- src/tower.gd
+++ src/tower.gd
@@ -2,9 +2,11 @@
 extends Node2D
 
 
-@export var is_player: bool = false:
+@export var is_player: bool:
 	set(value):
 		is_player = value
+		if !is_instance_valid($TowerArea) or !is_instance_valid($Area2D):
+			return
 		if is_player:
 			$TowerArea.scale.x = 1
 			$Area2D.scale.x = 1
@@ -21,8 +23,8 @@ var health: float = 1000:
 		health = value
 		if health <= 0.0:
 			health = 0.0
-		$TowerArea/PlayerHealthBar.value = health
-		$TowerArea/PlayerHealthBar/HealthValue.text = str(int(health))
+		$PlayerHealthBar.value = health
+		$PlayerHealthBar/HealthValue.text = str(int(health))
 
 @export var _tower_area_path: NodePath
 @onready var tower_area: Area2D = get_node(_tower_area_path)
@@ -30,11 +32,11 @@ var health: float = 1000:
 
 func _ready() -> void:
 	WS.new_data_received.connect(_on_WS_new_data_received)
+	is_player = is_player
 	tower_area.set_collision_layer_value(2, is_player)
 	tower_area.set_collision_layer_value(3, !is_player)
 	tower_area.set_collision_mask_value(2, !is_player)
 	tower_area.set_collision_mask_value(3, is_player)
-	scale.x *= -1 if !is_player else 1
 
 func _physics_process(_delta: float) -> void:
 	$"Area2D/SpawnAreaPreview".shape.size = spawn_range_y
