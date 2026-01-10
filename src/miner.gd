2fefc582b0d27dab52aef8cf484f1f9758004213
AdamMambetov <adammambetov.342@gmail.com>
2026-01-10 22:21:18 +0300

    refactor miner

--- src/miner.gd
+++ src/miner.gd
@@ -17,10 +17,7 @@ var selected_ore: Ore = null:
 		if is_instance_valid(selected_ore):
 			selected_ore.stop_select_anim()
 		selected_ore = value
-var route: Global.Route = Global.Route.Mine:
-	set(value):
-		route = value
-		update_direction()
+var route: Global.Route = Global.Route.Mine
 var max_bag_capacity: float = 0
 var bag_capacity: float = 0:
 	set(value):
@@ -37,88 +34,31 @@ var is_pressed: bool = false
 
 
 func _ready() -> void:
-	unit_state = UnitState.Idle
 	WS.new_data_received.connect(_on_WS_new_data_recieved)
-	update_direction()
+	attack_area.area_entered.connect(_on_attack_area_area_entered)
+	unit_state = UnitState.Idle
+	direction = get_default_direction()
 	stop_select_anim()
 
 func _physics_process(delta: float) -> void:
 	match unit_state:
-		UnitState.None:
-			if attack_area.has_overlapping_areas():
-				var areas = attack_area.get_overlapping_areas()
-				for area in areas:
-					# if tower
-					if area.get_collision_layer_value(4):
-						var tower = area.get_parent()
-						if route != Global.Route.Tower or !tower.is_player:
-							unit_state = UnitState.Walk
-							continue
-						elif get_tree().get_nodes_in_group(&"ore").is_empty():
-							unit_state = UnitState.Idle
-						WS.socket.send_text(JSON.stringify({
-							type = "sell_ore",
-							miner = id,
-						}))
-						bag_capacity = 0
-						route = Global.Route.Mine
-					# if ore
-					if area.get_collision_layer_value(5):
-						var ore = area.get_parent()
-						if route != Global.Route.Mine:
-							unit_state = UnitState.Walk
-							continue
-						if !is_instance_valid(ore):
-							continue
-						if ore.health <= 0:
-							continue
-						if !is_instance_valid(selected_ore) \
-								or selected_ore == ore:
-							current_ore = ore
-							unit_state = UnitState.Attack
-							break
-				if unit_state != UnitState.Attack:
-					unit_state = UnitState.Walk
-			elif route == Global.Route.Mine:
-				if !in_mine:
-					unit_state = UnitState.Walk
-					return
-				var ores = get_tree().get_nodes_in_group(&"ore")
-				if ores.is_empty():
-					route = Global.Route.Tower
-					unit_state = UnitState.Walk
-					return
-				
-				var nearest_ore: Node2D = ores.front()
-				if is_instance_valid(selected_ore):
-					nearest_enemy = selected_ore
-				else:
-					for ore in ores:
-						var ore_distance = global_position.distance_to(ore.global_position)
-						var nearest_distance = global_position.distance_to(nearest_ore.global_position)
-						if ore_distance < nearest_distance:
-							nearest_ore = ore
-				var to_direction = global_position.direction_to(nearest_ore.global_position)
-				if to_direction.x < 0:
-					direction = Vector2.LEFT
-				else:
-					direction = Vector2.RIGHT
-				unit_state = UnitState.Walk
-			else:
-				unit_state = UnitState.Walk
 		UnitState.Idle:
 			if !get_tree().get_nodes_in_group(&"ore").is_empty():
-				route = Global.Route.Mine
 				unit_state = UnitState.Walk
 		UnitState.Walk:
 			move_attack(delta)
-			if attack_area.has_overlapping_areas():
-				unit_state = UnitState.None
-				return
-			if get_tree().get_nodes_in_group(&"ore").is_empty():
-				unit_state = UnitState.None
-				return
 
+
+func move_attack(delta: float) -> void:
+	direction = get_default_direction()
+	if route == Global.Route.Mine and in_mine:
+		find_nearest_enemy()
+		if is_instance_valid(nearest_enemy):
+			direction = global_position.direction_to(nearest_enemy.global_position)
+			direction.x /= 2
+			direction.y *= 2
+			direction = direction.normalized()
+	position += speed * delta * direction
 
 func update_info(info: Dictionary) -> void:
 	super.update_info(info)
@@ -127,11 +67,26 @@ func update_info(info: Dictionary) -> void:
 	bag_capacity = info.bag_capacity
 
 func get_default_direction() -> Vector2:
-	update_direction()
+	match route:
+		Global.Route.Tower:
+			direction = Vector2.RIGHT
+			agr_collision.disabled = true
+		Global.Route.Mine:
+			direction = Vector2.LEFT
+			agr_collision.disabled = false
 	return direction
 
 func action_none() -> void:
 	unit_state = UnitState.None
+	find_nearest_enemy()
+	var areas = attack_area.get_overlapping_areas()
+	for area in areas:
+		_on_attack_area_area_entered(area)
+	if unit_state == UnitState.None:
+		action_move()
+
+func action_move() -> void:
+	unit_state = UnitState.Walk
 
 func find_nearest_enemy() -> void:
 	if !agr_area.has_overlapping_areas():
@@ -147,15 +102,6 @@ func find_nearest_enemy() -> void:
 			var distance = global_position.distance_to(enemy.global_position)
 			if distance < global_position.distance_to(nearest_enemy.global_position):
 				nearest_enemy = enemy
-
-func update_direction() -> void:
-	match route:
-		Global.Route.Tower:
-			direction = Vector2.RIGHT
-			agr_collision.disabled = true
-		Global.Route.Mine:
-			direction = Vector2.LEFT
-			agr_collision.disabled = false
 
 func play_select_anim() -> void:
 	selection_anim.visible = true
@@ -188,7 +134,7 @@ func _on_set_unit_state(_old: String, new: String) -> void:
 					to = current_ore.id,
 				}))
 			current_ore = null
-			unit_state = UnitState.None
+			action_none()
 
 func _on_set_direction(old: Vector2, new: Vector2) -> void:
 	super._on_set_direction(old, new)
@@ -209,6 +155,25 @@ func _on_WS_new_data_recieved(result: Dictionary) -> void:
 	if !result.has(id):
 		return
 	bag_capacity = result[id]
+
+func _on_attack_area_area_entered(area: Area2D) -> void:
+	var other = area.get_parent()
+	if other is Ore and route == Global.Route.Mine:
+		if !is_instance_valid(selected_ore) \
+				or selected_ore == other:
+			current_ore = other
+			unit_state = UnitState.Attack
+	if other is Tower and route == Global.Route.Tower:
+		if !other.is_player:
+			return
+		WS.socket.send_text(JSON.stringify({
+			type = "sell_ore",
+			miner = id,
+		}))
+		if get_tree().get_nodes_in_group(&"ore").is_empty():
+			unit_state = UnitState.Idle
+		bag_capacity = 0
+		route = Global.Route.Mine
 
 func _on_selection_area_input_event(
 		_viewport: Node,
